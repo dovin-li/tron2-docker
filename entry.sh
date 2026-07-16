e46c0e7dd32f0fd1c70d1f395c9e6d113d9da020
dovin-li <dovin.li@hypath.ai>
2026-07-16 17:26:20 +0800

    add mid360 TF to entry.sh, simplify start script

--- entry.sh
+++ entry.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.sh
 source /ws/install/setup.bash
-
-# start rosbridge (WebSocket gateway for ROS2 host)
 rosrun rosbridge_server rosbridge_websocket _port:=9090 &
-# start ground_truth_odom (odom->base_footprint TF)
 rosrun tron2_gazebo ground_truth_odom.py &
-# start static TF base_footprint->base_Link
 rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 base_footprint base_Link &
-
+rosrun tf2_ros static_transform_publisher 0 0 0.15 0 0 0 base_imu mid360_Link &
 exec "$@"
