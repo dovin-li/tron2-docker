9b76a51ef0659e76ec4ee5313f80146a029c5942
dovin-li <dovin.li@hypath.ai>
2026-07-19 04:05:14 +0800

    fix: gzclient crash (force_system_render=0) + mid360 LiDAR sensor plugin

--- entry.sh
+++ entry.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.sh
 source /ws/install/setup.bash
+
+# Fix for RTX 5060 Blackwell: force_system_render causes scene crash
+mkdir -p /root/.gazebo
+cat > /root/.gazebo/gui.ini << 'GUIEOF'
+[geometry]
+x=0
+y=0
+[rendering]
+force_system_render=0
+GUIEOF
+
 rosrun tron2_gazebo ground_truth_odom.py &
 rosrun tf2_ros static_transform_publisher 0 0 0 0 0 0 base_footprint base_link &
-rosrun tf2_ros static_transform_publisher 0 0 0.15 0 0 0 base_imu mid360_Link &
 sleep 5
 exec "$@"
-# v2
-# v3
